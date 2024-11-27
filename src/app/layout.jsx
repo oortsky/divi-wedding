7a97d99ae2b01e998ec63c8b8c445376418d3f7a
oortsky <bayuaprio22@gmail.com>
2024-11-27 09:03:51 +0700

    Next Push – Link Preview

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Poppins, Sacramento } from "next/font/google";
 import "./globals.css";
 
@@ -21,13 +22,13 @@ export const metadata = {
   openGraph: {
     title: "Divi Wedding — Official Invitation",
     description: "Undangan Pernikahan Nasrudin & Dewi Octaviani (Vivi).",
-    url: `${process.env.BASE_URL}`,
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
     siteName: "Divi Wedding — Official Invitation",
     images: [
       {
-        url: "images/preview.jpg",
-        width: 800,
-        height: 600,
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/images/preview.png`,
+        width: 1200,
+        height: 630,
         alt: "Preview Image"
       }
     ],
@@ -38,7 +39,7 @@ export const metadata = {
     card: "summary_large_image",
     title: "Divi Wedding — Official Invitation",
     description: "Undangan Pernikahan Nasrudin & Dewi Octaviani (Vivi).",
-    images: ["images/preview.jpg"]
+    images: [`${process.env.NEXT_PUBLIC_BASE_URL}/images/preview.png`]
   }
 };
 
@@ -50,6 +51,33 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${poppins.variable} ${sacramento.variable} font-poppins`}
     >
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta
+          property="og:description"
+          content={metadata.openGraph.description}
+        />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:site_name" content={metadata.openGraph.siteName} />
+        <meta property="og:image" content={metadata.openGraph.images[0].url} />
+        <meta
+          property="og:image:width"
+          content={metadata.openGraph.images[0].width}
+        />
+        <meta
+          property="og:image:height"
+          content={metadata.openGraph.images[0].height}
+        />
+        <meta property="twitter:card" content={metadata.twitter.card} />
+        <meta property="twitter:title" content={metadata.twitter.title} />
+        <meta
+          property="twitter:description"
+          content={metadata.twitter.description}
+        />
+        <meta property="twitter:image" content={metadata.twitter.images[0]} />
+      </Head>
       <body>{children}</body>
     </html>
   );
